9b037759ad8c4acc1df67938fac520af3340c6a3
Javier Burguete <jburguete@eead.csic.es>
2018-03-20 13:28:10 +0100

    Improving solutions documentation

--- 0.1.2/equation.c
+++ 0.1.2/equation.c
@@ -371,13 +371,13 @@ equation_acceleration_2 (Equation * eq, ///< Equation struct.
  * {\sqrt{g/\lambda}\,\cosh\left(\sqrt{g\,\lambda}\,t\right)
  * -\dot{z}_0\,\sinh\left(\sqrt{g\,\lambda}\,t\right)},\\
  * \dot{z}_0>0,\;
- * t\leq\frac{\arctan\left(\dot{z}_0\,\sqrt{\frac{\lambda}{g}}\right)}
+ * t\leq\frac{\arctan\left(\dot{z}_0\,\sqrt{\lambda/g}\right)}
  * {\sqrt{g\,\lambda}}\Rightarrow & 
  * \sqrt{\frac{g}{\lambda}}
  * \,\tan\left[\arctan\left(\dot{z}_0\,\sqrt{\frac{\lambda}{g}}\right)
  * -\sqrt{g\,\lambda}\,t\right],\\
  * \dot{z}_0>0,\;
- * t>\frac{\arctan\left(\dot{z}_0\,\sqrt{\frac{\lambda}{g}}\right)}
+ * t>\frac{\arctan\left(\dot{z}_0\,\sqrt{\lambda/g}\right)}
  * {\sqrt{g\,\lambda}}\Rightarrow & 
  * -\sqrt{\frac{g}{\lambda}}\,\tanh\left[\sqrt{g\,\lambda}\,t
  *  -\arctan\left(\dot{z}_0\,\sqrt{\frac{\lambda}{g}}\right)\right],
@@ -398,13 +398,13 @@ equation_acceleration_2 (Equation * eq, ///< Equation struct.
  * -\dot{z}_0\,\sqrt{\lambda/g}\,
  * \sinh\left(\sqrt{g\,\lambda}\,t\right)\right]}{\lambda},\\
  * \dot{z}_0>0,\;
- * t\leq\frac{\arctan\left(\dot{z}_0\,\sqrt{\frac{\lambda}{g}}\right)}
+ * t\leq\frac{\arctan\left(\dot{z}_0\,\sqrt{\lambda/g}\right)}
  * {\sqrt{g\,\lambda}}\Rightarrow & 
  * z_0+\frac{1}{\lambda}\,\ln\left\{\frac{\cos\left[
  * \arctan\left(\dot{z}_0\,\sqrt{\lambda/g}\right)-\sqrt{g\,\lambda}\,t\right]}
  * {\cos\left[\arctan\left(\dot{z}_0\,\sqrt{\lambda/g}\right)\right]}\right\},\\
  * \dot{z}_0>0,\;
- * t>\frac{\arctan\left(\dot{z}_0\,\sqrt{\frac{\lambda}{g}}\right)}
+ * t>\frac{\arctan\left(\dot{z}_0\,\sqrt{\lambda/g}\right)}
  * {\sqrt{g\,\lambda}}\Rightarrow & 
  * z_0-\frac{\ln\left\{
  * \cos\left[\arctan\left(\dot{z}_0\,\sqrt{\lambda/g}\right)\right]
